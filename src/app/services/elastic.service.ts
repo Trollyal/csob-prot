c327b92caf1137bcf4637ef62fe6d88547d7669e
Tomas Oktabec <oktabec@vshosting.cz>
2019-07-24 13:51:40 +0200

    search optim. + favicon & title

--- src/app/services/elastic.service.ts
+++ src/app/services/elastic.service.ts
@@ -33,41 +33,50 @@ export class ElasticService {
 
   getTransactions$(dates: { start: Date, end: Date }, search?: string, limit = 15) {
 
-    const must = search
-      ? [
-        {
-          query_string: {
-            query: `*${search || ''}*`
-          }
-        },
-        {
-          range: {
-            '@timestamp': {
-              gte: dates.start,
-              lte: dates.end
+    const bool = search
+      ? {
+        should: [
+            {
+              query_string: {
+                query: `*${ search || '' }*`
+              }
+            },
+            {
+              query_string: {
+                query: `${ search || '' }`
+              }
+            }
+          ],
+        must: [
+          {
+            range: {
+              '@timestamp': {
+                gte: dates.start,
+                lte: dates.end
+              }
             }
           }
-        }
-      ]
-      : [
-        {
-          range: {
-            '@timestamp': {
-              gte: dates.start,
-              lte: dates.end
+        ]
+      }
+      : {
+        must: [
+          {
+            range: {
+              '@timestamp': {
+                gte: dates.start,
+                lte: dates.end
+              }
             }
           }
-        }
-      ];
+        ]
+      };
 
     return this.http.post(
       '/api/txhistory/_search',
       {
         size: limit,
         query: {
-          bool: {
-            must
-          }
+          bool
         }
       }
     ).pipe(
